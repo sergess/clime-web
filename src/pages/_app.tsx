0e0aabf2b58b984fcc8a8bc2e58a1f0ecd15ea66
Siarhei Borodzich <siarhei.borodzich@apalon.com>
2021-07-26 11:45:34 +0300

    feat: [CLW-38] initial setup of the project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import type { AppProps } from 'next/app';
+
+import { ReactElement } from 'react';
+import { appWithTranslation } from 'next-i18next';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+const App = ({ Component, pageProps }: AppProps): ReactElement => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
+
+export default appWithTranslation(App);
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from 'react';
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { useTranslation } from 'next-i18next';
+
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export default function Home(): ReactElement {
+  const { t } = useTranslation('common');
+
+  return (
+    <div>
+      <Head>
+        <title>{t('Clime Web App')}</title>
+      </Head>
+
+      <main>
+        <h1>{t('Hello')}</h1>
+      </main>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(!!locale && (await serverSideTranslations(locale, ['common']))),
+  },
+});
