7cf11ce2d7d118e538ec21b14d6fe50ca2bbe4d6
Siarhei Borodzich <siarhei.borodzich@apalon.com>
2021-08-04 16:22:48 +0300

    feat: [CLW-46] add card component

---
+++ src/theme/components/card-component.theme.ts
@@ -0,0 +1,14 @@
+const Card = {
+  baseStyle: {
+    bg: 'white',
+    boxShadow: 'card',
+    display: 'flex',
+    flexDirection: 'column',
+    background: 'white',
+    alignItems: 'center',
+    position: 'relative',
+    borderRadius: '2xl',
+  },
+};
+
+export default Card;
--- src/theme/foundations/shadows-foundation.theme.ts
+++ src/theme/foundations/shadows-foundation.theme.ts
@@ -4,6 +4,7 @@ const shadows = {
   'page-link-text': '0px 1px 1px rgba(0, 0, 0, 0.1)',
   'hover-page-link-box': '0px 4px 8px rgba(26, 96, 179, 0.1)',
   'hover-clime-app-link-box': '0px 4px 8px rgba(179, 127, 26, 0.1)',
+  card: '0px 4px 8px rgba(26, 96, 179, 0.1), inset 0px -2px 0px rgba(60, 131, 232, 0.1)',
 };
 
 export default shadows;
