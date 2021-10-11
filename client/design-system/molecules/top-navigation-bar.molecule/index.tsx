818eff08e891e1960d7193fbe35cf7d339682f5d
Sergey Serzhan <sergey.serjan@apalon.com>
2021-10-11 08:33:36 +0300

    feat(nav): [CLW-44] Implement sub-header navigation UI elements + scroll to active item in navigation

--- client/design-system/molecules/top-navigation-bar.molecule/index.tsx
+++ client/design-system/molecules/top-navigation-bar.molecule/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useRef, useEffect } from 'react';
+import React, { ReactElement, useRef, useCallback } from 'react';
 import NextLink from 'next/link';
 import { Flex, Button } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
@@ -8,55 +8,60 @@ import { ClientOnly } from 'client/design-system/atoms';
 import { TopNavigationBarProps } from './types';
 
 export const TopNavigationBar = (): ReactElement => {
+  // [TODO]  ${countryCode}/${city}/${forecastZoneId} will be got from state
+
   const navItems: TopNavigationBarProps[] = [
     {
       label: 'Today',
-      url: '/weather-today/',
+      url: `/weather-today/`,
       type: 'common-nav-link',
     },
     {
       label: 'Hourly',
-      url: '/hourly-weather/',
+      url: `/hourly-weather/`,
       type: 'common-nav-link',
     },
     {
       label: 'Clime App',
-      url: '/',
+      url: '',
       type: 'app-nav-link',
     },
     {
       label: '10-day',
-      url: '/ten-day-weather/',
+      url: `/ten-day-weather/`,
       type: 'common-nav-link',
     },
     {
       label: 'Weather Radar',
-      url: '/weather-radar/',
+      url: `/weather-radar/`,
       type: 'common-nav-link',
     },
     {
       label: 'Alerts',
-      url: '/ten-day-weather/',
+      url: `/alerts/`,
       type: 'alert-nav-link',
     },
   ];
 
-  const menuCurrentItem = useRef(null);
   const router = useRouter();
 
-  useEffect(() => {
-    console.log(menuCurrentItem, router.pathname);
-    menuCurrentItem?.current?.scrollIntoView(true);
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const menuCurrentItemRef = useCallback((node) => {
+    if (node != null && navRef.current) {
+      const toCurrentMenuItem = node.getBoundingClientRect().left;
+      navRef.current.scrollLeft = toCurrentMenuItem;
+    }
   }, []);
 
   return (
     <ClientOnly>
-      <Flex as="nav" bg="gray.50" py="4" overflowX="auto">
+      <Flex as="nav" bg="gray.50" py="4" overflowX="auto" ref={navRef}>
         {navItems.map((item) => (
           <NextLink key={item.label} href={item.url} passHref>
             <Button
               as="a"
-              ref={router.pathname === item.url ? menuCurrentItem : null}
+              ref={router.pathname === item.url ? menuCurrentItemRef : null}
               flexShrink={0}
               aria-current={router.pathname === item.url && 'page'}
               variant={item.type}
