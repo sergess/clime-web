fb5d0680ae8b122af756ff93a1629e99df9e04e1
sergey_serjan <sergey.serjan@apalon.com>
2021-10-11 09:18:59 +0000

    Resolve CLW-54

--- client/design-system/molecules/index.ts
+++ client/design-system/molecules/index.ts
@@ -4,4 +4,5 @@ export * from './download.molecule';
 export * from './selectable-column-block.molecule';
 export * from './min-max-temperature-column.molecule';
 export * from './switch-selector.molecule';
+export * from './player.molecule';
 export * from './carousel.molecule';
