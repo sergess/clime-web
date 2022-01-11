5a4ae79cadf7011518da66714e60405196123dc1
sergey_serjan <sergey.serjan@apalon.com>
2022-01-11 12:19:39 +0000

    feat(500 page): template for 500 page

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,17 +1,18 @@
 import React, { ReactElement } from 'react';
 import { GetStaticProps } from 'next';
-import Link from 'next/link';
 import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { ErrorPageLayout } from 'client/design-system/templates';
 import { usePageUrl, useSetLocationDataByIp } from 'client/hooks';
 import { WEATHER_TODAY } from 'client/constants';
 
-const NotFoundPage = () => {
-  const { t } = useTranslation('page-404');
+const InternalServerErrorPage = () => {
+  const { t } = useTranslation('page-500');
+
   const pageUrl = usePageUrl(WEATHER_TODAY);
 
   const { locationData } = useSetLocationDataByIp();
@@ -29,22 +30,23 @@ const NotFoundPage = () => {
         bgSize="cover"
         bgPosition="center center"
       >
-        <Image src="/icons/404.svg" width={180} height={94} alt="404" />
-        <Text textStyle="24-bold" my="4">
-          {t('Page not found')}
+        <Image src="/icons/500.svg" width={180} height={94} alt="500" />
+        <Text textStyle="24-bold" mt="7">
+          {t('Internal Server Error')}
         </Text>
-        <Text textStyle="16-medium" align="center">
-          {t("There's nothing but wind here. Let's go to the home page.")}
+        <Text textStyle="16-medium" align="center" mt="4">
+          {t('Something went wrong')}
         </Text>
         <Link href={pageUrl} passHref>
           <Button
             variant="cta"
-            as="a"
-            mt="10"
+            mt="14"
             fontSize="14px"
+            minW="280px"
             disabled={!locationData}
+            as="a"
           >
-            {t("Explore today's weather")}
+            {t('Go Back')}
           </Button>
         </Link>
       </Flex>
@@ -55,14 +57,14 @@ const NotFoundPage = () => {
   );
 };
 
-export default NotFoundPage;
+export default InternalServerErrorPage;
 
-NotFoundPage.getLayout = function getLayout(page: ReactElement) {
+InternalServerErrorPage.getLayout = function getLayout(page: ReactElement) {
   return <ErrorPageLayout>{page}</ErrorPageLayout>;
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
-    ...(!!locale && (await serverSideTranslations(locale, ['page-404']))),
+    ...(!!locale && (await serverSideTranslations(locale, ['page-500']))),
   },
 });
