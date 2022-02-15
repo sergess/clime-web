b4c44800d702abae75e1f4adb06d85460c3ff80d
Siarhei Borodzich <siarhei.borodzich@apalon.com>
2022-02-15 12:28:24 +0300

    fix: [CLW-99] remove focus on slider after onChange event

--- client/design-system/molecules/slider-player.molecule/index.tsx
+++ client/design-system/molecules/slider-player.molecule/index.tsx
@@ -8,8 +8,7 @@ import {
   IconButton,
 } from '@chakra-ui/react';
 import Image from 'next/image';
-
-import type { FC, ReactElement, ReactNode } from 'react';
+import { FC, ReactElement, ReactNode, useMemo } from 'react';
 import type { SliderProps } from '@chakra-ui/react';
 
 export const SliderPlayer: FC<
@@ -32,96 +31,104 @@ export const SliderPlayer: FC<
   endLabel,
   onChange,
   onChangeEnd,
-}): ReactElement => (
-  <Flex
-    align="stretch"
-    h="48px"
-    ps="4"
-    pe="2.5"
-    bg="white"
-    borderRadius="xl"
-    maxW={{ md: 380 }}
-    margin={{ md: '0 auto' }}
-  >
-    <Flex align="center" pe="3">
-      <IconButton
-        variant="player-button"
-        aria-label={playing ? 'Play' : 'Pause'}
-        icon={
-          <Image
-            src={`/icons/${playing ? 'pause' : 'play'}.svg`}
-            width={24}
-            height={24}
-            alt={playing ? 'Playing' : 'Paused'}
-          />
-        }
-        onClick={onPlayToggle}
+}): ReactElement => {
+  const stepMarks = useMemo(() => {
+    const steps = Array.from(Array(max).keys());
+
+    return steps.map((v, i) => (
+      <SliderMark
+        key={v}
+        value={i * (step as number)}
+        bg="gray.100"
+        w={0.5}
+        // [todo] move zIndex to constants ABOVE_THE_MAP, MAP, SETTINGS, еtс
+        zIndex={1}
+        h={1}
+        mt={-0.5}
+        ms="-1px"
       />
-    </Flex>
+    ));
+  }, [max, step]);
+
+  return (
     <Flex
-      justify="flex-end"
-      w="full"
-      my="2"
-      position="relative"
-      flexDirection="column"
+      align="stretch"
+      h="48px"
+      ps="4"
+      pe="2.5"
+      bg="white"
+      borderRadius="xl"
+      maxW={{ md: 380 }}
+      margin={{ md: '0 auto' }}
     >
-      <Flex direction="column">
-        <Flex w="full" justify="space-between" align="center" direction="row">
-          <Flex>{startLabel && startLabel}</Flex>
-
-          <Flex>{nowLabel && nowLabel}</Flex>
-
-          <Flex>{endLabel && endLabel}</Flex>
-        </Flex>
-
-        <Flex mt="3px" mb="-3px" direction="row">
-          {endLabel && (
-            <Flex
-              w={0}
-              h={0}
-              ms="auto"
-              borderRadius="xl"
-              borderLeft="3px solid"
-              borderRight="3px solid"
-              borderBottom="5px solid"
-              borderColor="transparent"
-              borderBottomColor="gray.100"
+      <Flex align="center" pe="3">
+        <IconButton
+          variant="player-button"
+          aria-label={playing ? 'Play' : 'Pause'}
+          icon={
+            <Image
+              src={`/icons/${playing ? 'pause' : 'play'}.svg`}
+              width={24}
+              height={24}
+              alt={playing ? 'Playing' : 'Paused'}
             />
-          )}
-        </Flex>
+          }
+          onClick={onPlayToggle}
+        />
       </Flex>
-
-      <Slider
-        step={step}
-        min={min}
-        max={max}
-        value={value}
-        onChange={onChange}
-        onChangeEnd={onChangeEnd}
+      <Flex
+        justify="flex-end"
+        w="full"
+        my="2"
+        position="relative"
+        flexDirection="column"
       >
-        {/* [todo] refactor this */}
-        {Array.from(Array(max).keys()).map((v, i) => (
-          <SliderMark
-            key={v}
-            value={i * (step as number)}
-            bg="gray.100"
-            w={0.5}
-            // [todo] move zIndex to constants ABOVE_THE_MAP, MAP, SETTINGS, еtс
-            zIndex={1}
-            h={1}
-            mt={-0.5}
-            ms="-1px"
-          />
-        ))}
+        <Flex direction="column">
+          <Flex w="full" justify="space-between" align="center" direction="row">
+            <Flex>{startLabel && startLabel}</Flex>
 
-        <SliderTrack bg="gray.100">
-          <SliderFilledTrack bg="blue.100" />
-        </SliderTrack>
+            <Flex>{nowLabel && nowLabel}</Flex>
 
-        <SliderThumb boxSize={4} bg="blue.500" />
-      </Slider>
+            <Flex>{endLabel && endLabel}</Flex>
+          </Flex>
+
+          <Flex mt="3px" mb="-3px" direction="row">
+            {endLabel && (
+              <Flex
+                w={0}
+                h={0}
+                ms="auto"
+                borderRadius="xl"
+                borderLeft="3px solid"
+                borderRight="3px solid"
+                borderBottom="5px solid"
+                borderColor="transparent"
+                borderBottomColor="gray.100"
+              />
+            )}
+          </Flex>
+        </Flex>
+
+        <Slider
+          step={step}
+          min={min}
+          max={max}
+          value={value}
+          onChange={onChange}
+          onChangeEnd={onChangeEnd}
+          focusThumbOnChange={false}
+        >
+          {stepMarks}
+
+          <SliderTrack bg="gray.100">
+            <SliderFilledTrack bg="blue.100" />
+          </SliderTrack>
+
+          <SliderThumb boxSize={4} bg="blue.500" />
+        </Slider>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
 
 export default SliderPlayer;
