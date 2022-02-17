87a1598ad6481810d2e67dfbcac9bdab6ecd150e
Siarhei Borodzich <siarhei.borodzich@apalon.com>
2022-02-17 15:48:03 +0300

    refactor(sitemap): [CLW-258] make sitemap page url dynamic

--- pages/sitemaps/[name].ts
+++ pages/sitemaps/[name].ts
@@ -5,10 +5,13 @@ export const Sitemap = (): void => {};
 export default Sitemap;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const request = await fetch(process.env.SITEMAP_URL as string);
+  const request = await fetch(
+    `${process.env.SITEMAP_BASE_URL}/${context?.query?.name}`
+  );
   const body = await request.text();
 
   context.res.setHeader('Content-Type', 'application/xml');
+  context.res.setHeader('Cache-Control', 'public, max-age=86400'); // 24 hours
   context.res.write(body);
   context.res.end();
 
