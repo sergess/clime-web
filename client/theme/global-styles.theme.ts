571df1645f7ff96033e76a0419b87752b689a135
Hanna Kaltovich <hanna.kaltovich@apalon.com>
2022-07-27 16:23:06 +0400

    fix: [WT-1690] cookie banner appearance

--- client/theme/global-styles.theme.ts
+++ client/theme/global-styles.theme.ts
@@ -9,6 +9,36 @@ const styles = {
       flexDirection: 'column',
       h: '100%',
     },
+    '#onetrust-button-group button': {
+      backgroundColor: '#1c72eb !important',
+      borderRadius: '4px !important',
+      border: 'none !important',
+      fontWeight: '600 !important',
+      fontSize: '14px !important',
+      lineHeight: '18px !important',
+      marginBottom: '12px !important',
+    },
+    '#onetrust-policy #onetrust-policy-title': {
+      fontWeight: '600 !important',
+      fontSize: '18px !important',
+      lineHeight: '20px !important',
+      color: '#333 !important',
+      marginBottom: '0 !important',
+    },
+    '#onetrust-policy #onetrust-policy-text': {
+      fontWeight: '400 !important',
+      fontSize: '14px !important',
+      lineHeight: '18px !important',
+      color: '#333 !important',
+      paddingTop: '8px !important',
+    },
+    '#onetrust-banner-sdk #onetrust-policy': {
+      marginTop: '20px !important',
+    },
+    '.ot-floating-button__front': {
+      backgroundImage: 'url(/icon_cookies.png) !important',
+      boxShadow: '0 4px 4px 0 rgba(0, 0, 0, 0.1) !important',
+    },
   },
 };
 
