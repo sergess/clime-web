d18ae245f371ed9058ba38f81579ba76320eb356
sergey_serjan <serguei.serjan@apalon.com>
2023-12-01 12:43:30 +0300

    feat(clime-app): [WT-2129] changed footer store link

--- client/design-system/molecules/download.molecule/index.tsx
+++ client/design-system/molecules/download.molecule/index.tsx
@@ -2,17 +2,33 @@ import React, { ReactElement } from 'react';
 import { Link } from '@chakra-ui/react';
 import { isMobile as mobile, isIOS as ios } from 'react-device-detect';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-import { IOS_STORE_LINK, ANDROID_STORE_LINK } from 'client/constants';
+import {
+  IOS_STORE_LINK,
+  ANDROID_STORE_LINK,
+  STORE_LINK_UAC,
+} from 'client/constants';
 
 export const Download = (): ReactElement => {
+  const router = useRouter();
+  const isSomePage = router?.asPath === '/climeapp';
+
   if (mobile) {
     return ios ? (
-      <Link href={IOS_STORE_LINK} aria-label="App store" isExternal>
+      <Link
+        href={isSomePage ? STORE_LINK_UAC : IOS_STORE_LINK}
+        aria-label="App store"
+        isExternal
+      >
         <Image src="/icons/app-store.svg" width={120} height={40} alt="" />
       </Link>
     ) : (
-      <Link href={ANDROID_STORE_LINK} aria-label="Google play" isExternal>
+      <Link
+        href={isSomePage ? STORE_LINK_UAC : ANDROID_STORE_LINK}
+        aria-label="Google play"
+        isExternal
+      >
         <Image src="/icons/google-play.svg" width={136} height={40} alt="" />
       </Link>
     );
@@ -20,10 +36,19 @@ export const Download = (): ReactElement => {
 
   return (
     <>
-      <Link href={IOS_STORE_LINK} aria-label="App store" me="5" isExternal>
+      <Link
+        href={isSomePage ? STORE_LINK_UAC : IOS_STORE_LINK}
+        aria-label="App store"
+        me="5"
+        isExternal
+      >
         <Image src="/icons/app-store.svg" width={120} height={40} alt="" />
       </Link>
-      <Link href={ANDROID_STORE_LINK} aria-label="Google play" isExternal>
+      <Link
+        href={isSomePage ? STORE_LINK_UAC : ANDROID_STORE_LINK}
+        aria-label="Google play"
+        isExternal
+      >
         <Image src="/icons/google-play.svg" width={136} height={40} alt="" />
       </Link>
     </>
